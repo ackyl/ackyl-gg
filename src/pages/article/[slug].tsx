af5d8dc3594d33256dc66fd5fbf09df1b5070def
Ackyl <ackylmohamad@gmail.com>
2023-03-27 11:36:19 +0700

    fix image loader by adding next config, hide article slug for now

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -16,57 +16,55 @@ interface IParams extends ParsedUrlQuery {
 }
 
 export default function ArticlePage(props: Props) {
-  const {
-    frontmatter: { title, subtitle },
-    content,
-  } = props.article;
+  // const {
+  //   frontmatter: { title, subtitle },
+  //   content,
+  // } = props.article;
 
-  return (
-    <div className="article">
-      <div className="article-header">
-        <div className="article-header__top">
-          <div className="article-header__link">
-            <img alt="" src="/left-arrow.svg" />
-            <Link href="/"></Link>
-          </div>
-          <h1>{title}</h1>
-        </div>
-        <p>{subtitle}</p>
-      </div>
+  // return (
+  //   <div className="article">
+  //     <div className="article-header">
+  //       <div className="article-header__top">
+  //         <div className="article-header__link">
+  //           <img alt="" src="/left-arrow.svg" />
+  //           <Link href="/"></Link>
+  //         </div>
+  //         <h1>{title}</h1>
+  //       </div>
+  //       <p>{subtitle}</p>
+  //     </div>
 
-      <div
-        className="article-content"
-        dangerouslySetInnerHTML={{ __html: marked(content) }}
-      ></div>
-    </div>
-  );
+  //     <div
+  //       className="article-content"
+  //       dangerouslySetInnerHTML={{ __html: marked(content) }}
+  //     ></div>
+  //   </div>
+  // );
+
+  return <div></div>;
 }
 
-export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join("data/articles"));
+// export async function getStaticPaths() {
+//   // const files = fs.readdirSync(path.join("src/data/articles"));
+//   // const paths = files.map((filename) => ({
+//   //   params: {
+//   //     slug: filename.replace(".md", ""),
+//   //   },
+//   // }));
+//   // return {
+//   //   paths,
+//   //   fallback: false,
+//   // };
+// }
 
-  const paths = files.map((filename) => ({
-    params: {
-      slug: filename.replace(".md", ""),
-    },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps(context: { params: IParams }) {
-  const { slug } = context.params;
-  const markdownWithMeta = fs.readFileSync(
-    path.join("data/articles", slug + ".md"),
-    "utf-8"
-  );
-
-  const { data: frontmatter, content } = matter(markdownWithMeta);
-
-  return {
-    props: { frontmatter, slug, content },
-  };
-}
+// export async function getStaticProps(context: { params: IParams }) {
+//   // const { slug } = context.params;
+//   // const markdownWithMeta = fs.readFileSync(
+//   //   path.join("src/data/articles", slug + ".md"),
+//   //   "utf-8"
+//   // );
+//   // const { data: frontmatter, content } = matter(markdownWithMeta);
+//   // return {
+//   //   props: { frontmatter, slug, content },
+//   // };
+// }
